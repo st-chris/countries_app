93ca768c9bfcb8f98f97820b9d59c10489129400
st-chris <stchrisdev@gmail.com>
2019-07-08 15:00:55 +0200

    Updates

---
+++ precache-manifest.bf5791fa191c787d374a7f2493aae381.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93a2d59e013ffe84fb1ab63670024abb",
+    "url": "/countries/index.html"
+  },
+  {
+    "revision": "19e16060d214edfd7323",
+    "url": "/countries/static/css/main.d1c840a3.chunk.css"
+  },
+  {
+    "revision": "1db416169f9218a4ad2a",
+    "url": "/countries/static/js/2.71566004.chunk.js"
+  },
+  {
+    "revision": "19e16060d214edfd7323",
+    "url": "/countries/static/js/main.ecfaf086.chunk.js"
+  },
+  {
+    "revision": "84c1202aabc4d9b174af",
+    "url": "/countries/static/js/runtime~main.30bae189.js"
+  }
+]);
